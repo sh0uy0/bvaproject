b67246cfc33faa98fa86e905248dee2aa4f52e6f
sh0uy0 <44997862+sh0uy0@users.noreply.github.com>
2020-06-09 17:14:41 +0300

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -1,15 +1,52 @@
-(function($){
-  $(function(){
+//Sidenav Initialization
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {
+    edge: 'right',
+    draggable: true,
+    preventscrolling: true
+  });
+});
 
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-    $('.carousel.carousel-slider').carousel({
-      indicators: true,
-      fullWidth: true
-    });
-    $('.carousel1.carousel-slider1').carousel({
-      fullWidth: true,
-      indicators: true
-    });
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+//Parallax Initialization
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems);
+});
+
+//Carousel Initialization
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel.carousel-slider');
+  var instances = M.Carousel.init(elems, {
+    fullWidth: true,
+    indicators: true
+  });
+});
+
+//Floating Action Button Initialization
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    hoverEnabled: false
+  });
+});
+
+//Tool Tip Initialization
+document.addEventListener('DOMContentLoaded', function() {
+  var toolTiphtml = '<span class="white-text islightlypadded blue-grey">Scroll to Top</span>'
+  var elems = document.querySelectorAll('.tooltipped');
+  var instances = M.Tooltip.init(elems, {
+      exitDelay: 100,
+      position: 'left',
+      html: toolTiphtml
+  });
+});
+
+//Toast Initialization
+function handleClick (){
+  var toastHTML = '<span>Success!:)</span>';
+  M.toast({
+    html: toastHTML,
+    classes: 'smooth blue-grey'
+  });
+}
